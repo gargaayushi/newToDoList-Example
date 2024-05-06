e5f23c7d84de14e8f3b8484a7285346abac725c4
Aayushi Garg <aayushigarg17@gmail.com>
2024-05-05 21:29:40 -0700

    Complete Iteration 0

---
+++ NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  newToDoList
+//
+//  Created by Aayushi Garg on 5/5/24.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
