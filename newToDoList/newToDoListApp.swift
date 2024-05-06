be3da26a732aed01ad7f38a69dab86cebc659d90
Aayushi Garg <aayushigarg17@gmail.com>
2024-05-05 21:13:02 -0700

    Initial Commit

---
+++ newToDoList/newToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  newToDoListApp.swift
+//  newToDoList
+//
+//  Created by Aayushi Garg on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct newToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
